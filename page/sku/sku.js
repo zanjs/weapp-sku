5a34a4800df606d73aef6bf6ab927fd9d0e1b7ce
zanjs <root@zanjs.com>
2016-12-25 16:57:42 +0800

    Those who turn back never reach the summit. :tulip:

--- page/sku/sku.js
+++ page/sku/sku.js
@@ -1,20 +1,209 @@
+import api from '../../util/api.js';
+
 Page({
-  data:{
-    text:"Page sku"
+  data: {
+    Skus: [{
+      "SkuId": "B00TSUGXKE",
+      "Title": "Fire",
+      "Price": 39.99,
+      "ListPrice": 49.99,
+      "Freight": 0.0,
+      "Quantity": 30,
+      "PropIds": ["0:0", "2:0", "1:0"],
+      "Selected": true
+    }, {
+      "SkuId": "B00ZDWGF7W",
+      "Title": "Fire",
+      "Price": 54.99,
+      "ListPrice": 64.99,
+      "Freight": 0.0,
+      "Quantity": 30,
+      "PropIds": ["0:0", "2:1", "1:0"],
+
+      "Selected": false
+    }, {
+      "SkuId": "B018Y227MY",
+      "Title": "Fire",
+      "Price": 39.99,
+      "ListPrice": 49.99,
+      "Freight": 0.0,
+      "Quantity": 30,
+      "PropIds": ["0:0", "2:0", "1:1"],
+
+      "Selected": false
+    }, {
+      "SkuId": "B018Y229OU",
+      "Title": "Fire",
+      "Price": 39.99,
+      "ListPrice": 49.99,
+      "Freight": 0.0,
+      "Quantity": 30,
+      "PropIds": ["0:0", "2:0", "1:2"],
+
+      "Selected": false
+    }, {
+      "SkuId": "B018Y23P7K",
+      "Title": "Fire",
+      "Price": 39.99,
+      "ListPrice": 49.99,
+      "Freight": 0.0,
+      "Quantity": 30,
+      "PropIds": ["0:0", "2:0", "1:3"],
+
+      "Selected": false
+    }, {
+      "SkuId": "B018Y22BI4",
+      "Title": "Fire",
+      "Price": 49.99,
+      "ListPrice": 69.99,
+      "Freight": 0.0,
+      "Quantity": 30,
+      "PropIds": ["0:1", "2:0", "1:0"],
+
+      "Selected": false
+    }, {
+      "SkuId": "B018Y229K4",
+      "Title": "Fire",
+      "Price": 64.99,
+      "ListPrice": 84.99,
+      "Freight": 0.0,
+      "Quantity": 30,
+      "PropIds": ["0:1", "2:1", "1:0"],
+
+      "Selected": false
+    }, {
+      "SkuId": "B018Y225IA",
+      "Title": "Fire",
+      "Price": 49.99,
+      "ListPrice": 69.99,
+      "Freight": 0.0,
+      "Quantity": 30,
+      "PropIds": ["0:1", "2:0", "1:1"],
+
+      "Selected": false
+    }, {
+      "SkuId": "B018Y224PY",
+      "Title": "Fire",
+      "Price": 49.99,
+      "ListPrice": 69.99,
+      "Freight": 0.0,
+      "Quantity": 30,
+      "PropIds": ["0:1", "2:0", "1:2"],
+      "Selected": false
+
+    }, {
+      "SkuId": "B018Y227A6",
+      "Title": "Fire",
+      "Price": 49.99,
+      "ListPrice": 69.99,
+      "Freight": 0.0,
+      "Quantity": 30,
+      "PropIds": ["0:1", "2:0", "1:3"],
+      "Selected": false
+    }],
+    SkuClasses: [{
+      "TypeName": "DigitalStorageCapacity",
+      "SkuProperties": [{
+        "PropId": "0:0",
+        "PropertieName": "8 GB",
+      }, {
+        "PropId": "0:1",
+        "PropertieName": "16 GB",
+      }]
+    }, {
+      "TypeName": "Color",
+      "SkuProperties": [{
+        "PropId": "1:0",
+        "PropertieName": "Black",
+      }, {
+        "PropId": "1:1",
+        "PropertieName": "Blue",
+
+      }, {
+        "PropId": "1:2",
+        "PropertieName": "Magenta",
+      }, {
+        "PropId": "1:3",
+        "PropertieName": "Tangerine",
+      }]
+    }, {
+      "TypeName": "Configuration",
+      "SkuProperties": [{
+        "PropId": "2:0",
+        "PropertieName": "With Special Offers"
+      }, {
+        "PropId": "2:1",
+        "PropertieName": "Without Special Offers"
+      }]
+    }],
+    // 保存最后的组合结果信息
+    SKUResult: {}
   },
-  onLoad:function(options){
+  skuClick(event) {
+    let vm = this
+    let pid = vm.evevt(event, 'id')
+    console.log(pid)
+  },
+  //把组合的key放入结果集SKUResult
+  add2SKUResult(combArrItem, sku) {
+    var key = combArrItem.join(";");
+    if (SKUResult[key]) { //SKU信息key属性·
+      SKUResult[key].count += sku.Quantity;
+      SKUResult[key].prices.push(sku.Price);
+    } else {
+      SKUResult[key] = {
+        count: sku.Quantity,
+        prices: [sku.Price]
+      };
+    }
+  },
+  //初始化得到结果集
+  initSKU(data) {
+    var i, j, skuKeys = getObjKeys(data);
+    for (i = 0; i < skuKeys.length; i++) {
+      var skuKey = skuKeys[i]; //一条SKU信息key
+      var sku = data[skuKey]; //一条SKU信息value
+      var skuKeyAttrs = skuKey.split(";"); //SKU信息key属性值数组
+      skuKeyAttrs.sort(function (value1, value2) {
+        return parseInt(value1) - parseInt(value2);
+      });
+
+      //对每个SKU信息key属性值进行拆分组合
+      var combArr = combInArray(skuKeyAttrs);
+      for (j = 0; j < combArr.length; j++) {
+        add2SKUResult(combArr[j], sku);
+      }
+
+      //结果集接放入SKUResult
+      SKUResult[skuKeyAttrs.join(";")] = {
+        count: sku.Quantity,
+        prices: [sku.Price]
+      }
+    }
+  },
+  evevt(event, key) {
+    return event.currentTarget.dataset[key]
+  },
+  init() {
+    let vm = this
+    let skus = vm.data.Skus
+    var indata = SkusObj(skus)
+
+    initSKU(indata)
+  },
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   }
 })
---
+++ util/api.js
@@ -0,0 +1,102 @@
+'use strict';
+
+
+export default {
+    SkusObj(arr) {
+        var skus = arr;
+        var leng = skus.length;
+        var newObj = {};
+        for (var i = 0; i < leng; i++) {
+            var item = skus[i];
+            var key = item.PropIds.join(";");
+            newObj[key] = {};
+            newObj[key].SkuId = item.SkuId;
+            newObj[key].Price = item.Price;
+            newObj[key].ListPrice = item.ListPrice;
+            newObj[key].Quantity = item.Quantity;
+            newObj[key].Selected = item.Selected;
+
+        }
+        console.log(newObj);
+        return newObj
+    },
+    //获得对象的key
+    getObjKeys(obj) {
+        if (obj !== Object(obj)) throw new TypeError('Invalid object');
+        var keys = [];
+        for (var key in obj)
+            if (Object.prototype.hasOwnProperty.call(obj, key))
+                keys[keys.length] = key;
+        return keys;
+    },
+    /**
+     * 从数组中生成指定长度的组合
+     * 方法: 先生成[0,1...]形式的数组, 然后根据0,1从原数组取元素，得到组合数组
+     */
+    combInArray(aData) {
+        if (!aData || !aData.length) {
+            return [];
+        }
+
+        var len = aData.length;
+        var aResult = [];
+
+        for (var n = 1; n < len; n++) {
+            var aaFlags = getCombFlags(len, n);
+            while (aaFlags.length) {
+                var aFlag = aaFlags.shift();
+                var aComb = [];
+                for (var i = 0; i < len; i++) {
+                    aFlag[i] && aComb.push(aData[i]);
+                }
+                aResult.push(aComb);
+            }
+        }
+
+        return aResult;
+    },
+    /**
+     * 得到从 m 元素中取 n 元素的所有组合
+     * 结果为[0,1...]形式的数组, 1表示选中，0表示不选
+     */
+    getCombFlags(m, n) {
+        if (!n || n < 1) {
+            return [];
+        }
+
+        var aResult = [];
+        var aFlag = [];
+        var bNext = true;
+        var i, j, iCnt1;
+
+        for (i = 0; i < m; i++) {
+            aFlag[i] = i < n ? 1 : 0;
+        }
+
+        aResult.push(aFlag.concat());
+
+        while (bNext) {
+            iCnt1 = 0;
+            for (i = 0; i < m - 1; i++) {
+                if (aFlag[i] == 1 && aFlag[i + 1] == 0) {
+                    for (j = 0; j < i; j++) {
+                        aFlag[j] = j < iCnt1 ? 1 : 0;
+                    }
+                    aFlag[i] = 0;
+                    aFlag[i + 1] = 1;
+                    var aTmp = aFlag.concat();
+                    aResult.push(aTmp);
+                    if (aTmp.slice(-n).join("").indexOf('0') == -1) {
+                        bNext = false;
+                    }
+                    break;
+                }
+                aFlag[i] == 1 && iCnt1++;
+            }
+        }
+        return aResult;
+    },
+    event(event, key) {
+        return event.currentTarget.dataset[key]
+    }
+}
